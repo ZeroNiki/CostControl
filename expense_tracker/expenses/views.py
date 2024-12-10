1da789adccb9100552e59b716436030e0a1e2df6
Nikita Menshov <nikitatop2222@gmail.com>
2024-12-10 17:16:53 +0500

    add API routers

--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Category, Transaction
+from .serializers import CategorySerializers, TransactionSerializers 
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializers 
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
