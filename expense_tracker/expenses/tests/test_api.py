77c1e611a04dc2428b3dcce36b45327bbc5d4abf
Nikita Menshov <nikitatop2222@gmail.com>
2024-12-12 16:24:17 +0500

    Add tests

---
+++ expense_tracker/expenses/tests/test_api.py
@@ -0,0 +1,50 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from django.utils import timezone
+from django.contrib.auth import get_user_model 
+
+from expenses.models import Category
+
+
+class CategoryAPITest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="testuser", email="testuser@example.com", password="testpass")
+
+        self.refresh = RefreshToken.for_user(self.user)
+        self.access_token = str(self.refresh.access_token)
+
+
+    def test_create_category(self):
+        url = "http://127.0.0.1:8000/api/categories/"
+        data = {"name": "Transport"}
+        response = self.client.post(url, data, HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["name"], "Transport")
+
+
+class TransactionAPITest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="testuser", email="testuser@example.com", password="testpass")
+        self.category = Category.objects.create(name="Transport", user=self.user)
+
+        self.refresh = RefreshToken.for_user(self.user)
+        self.access_token = str(self.refresh.access_token)
+
+
+    def test_create_transaction(self):
+        url = "http://127.0.0.1:8000/api/transactions/"
+        data = {
+            "category": self.category.id,
+            "amount": "-50.00", 
+            "description": "Taxi ride", 
+            "date": timezone.now().isoformat() 
+        }
+        response = self.client.post(url, data, HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        self.assertEqual(response.data["category"], self.category.id)
+        self.assertEqual(str(response.data["amount"]), str(data["amount"]))
+        self.assertEqual(response.data["description"], data["description"])
---
+++ expense_tracker/expenses/tests/test_model.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+from expenses.models import Category, Transaction
+
+
+class CategoryModelTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+                username="testuser", 
+                email="testuser@example.com", 
+                password="testpass"
+        )
+
+        self.category = Category.objects.create(name="Transport", user=self.user)
+
+
+    def test_category_str(self):
+        self.assertEqual(str(self.category), "Transport")
+
+
+class TransactionModelTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+               username="testuser",
+               email="testuser@example.com",
+               password="testpass"
+        ) 
+
+        self.category = Category.objects.create(name="Transport", user=self.user)
+
+        self.transaction = Transaction.objects.create(
+                category=self.category,
+                amount=-50,
+                description="Taxi ride",
+                date=timezone.now().isoformat() ,
+                user=self.user
+        )
+
+
+    def test_transaction_str(self):
+        self.assertEqual(str(self.transaction.category), str(self.category.name))
+        self.assertEqual(self.transaction.amount, -50)
+        self.assertEqual(str(self.transaction.description), "Taxi ride")
